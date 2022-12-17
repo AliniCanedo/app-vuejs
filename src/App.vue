d3cb449a1784ec241e2c48a41f733ebb6a171068
Alini Canedo <alini.canedo@icertus.com.br>
2022-12-17 13:20:43 -0300

    listing contacts and add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,59 @@
     <a href="/">Agenda</a>
     <div>
       <span>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#dedede" class="bi bi-search" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="#dedede"
+          class="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+          />
         </svg>
       </span>
       <input type="text" placeholder="Pesquisar..." />
     </div>
   </header>
-
   <main>
-    
-
+    <div class="container">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">E-mail</th>
+          </tr>
+        </thead>
+        <template v-for="(contact, index) in this.contacts" :key="index">
+          <tbody>
+            <tr>
+              <th scope="row">{{ contact.id }}</th>
+              <td>{{ contact.name }}</td>
+              <td>{{ contact.email }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </table>
+    </div>
   </main>
 </template>
 
 <script>
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 export default {
   name: "App",
   data() {
-    return { }
+    return {
+      contacts: {},
+    };
   },
   created() {
     this.axios.get("http://localhost:3000/contacts").then((response) => {
-      console.log(response.data);
+      this.contacts = response.data;
     });
   },
 };
